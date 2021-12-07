1b30d72a14ab46ebea36e1ed0af727c58bfdae11
Isanfurg <sanfurgoh@gmail.com>
2021-12-06 22:10:18 -0300

    Delete Comments

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -9,24 +9,15 @@ import { MatGridList} from '@angular/material/grid-list';
 })
 export class ArticlesComponent implements OnInit {
 
-  //variable que mantiene la cantidad de articulos por página
   public offset: number;
-  //contiene los ID de todos los artículos asociados a los top 200 (esto ya que no necesariamente podrian ser 200,
-  // eso depende de la API actual)
   public allArticlesID: number[];
   
-  //contiene los ID de los artículos de la página actual
   public articlesID: number[];
 
-  //marca para definir que el listado de articulos de la página actual están cargados correctamente
   public isReady: boolean;
 
-  //marca para definir un problema con la carga de datos del listado de articulos de la página actual
-  //marca para definir un problema con la carga de datos del listado de articulos de la página actual
-
+  
   public flagError!: boolean;
-
-  //contador usado por el método changeIsLoading() para monitorear el estado de los hijos (articulo del listado)
   public countChargeArticles: number;
 
   constructor(private service: ApiService,
@@ -50,12 +41,7 @@ export class ArticlesComponent implements OnInit {
     );
   }
 
-  /**
-   * Método para obtener el estado de los items del listado de artículos:
-   *  Si todos llegan a ser exitosos, se muestran al usuario y se oculta el icono de loading
-   *  En caso contrario se muestra un mensaje de error.
-   * @param event - contiene la respuesta de uno de los artículos (1 - exitoso / 2 - error).
-   */
+  
   changeIsLoading(event: string | number){
     if(+event == 1){
       this.countChargeArticles++;
@@ -69,11 +55,7 @@ export class ArticlesComponent implements OnInit {
     }
   }
 
-  /**
-   * Método para realizar la actualización de la lista de artículos en base a paginación.
-   * La cantidad de elementos por página se define en la variable offset, definido en el constructor.
-   * @param index - índice de la paginación requerida.
-   */
+ 
   onPaginateChange(index: number){
     this.flagError = false;
     this.isReady = false;
